d0d5536c82013d9d78d024bd9edd265ed8e556d1
richardvanderdys <richard.vanderdys@smarterdx.com>
2023-02-25 15:25:45 -0600

    calculating fps

--- src/components/physics/Simulation.ts
+++ src/components/physics/Simulation.ts
@@ -9,6 +9,8 @@ export default class Simulation {
   private solver = new Solver();
   private shouldStop = false;
   private lastSpawn = Date.now();
+  private times: number[] = [];
+  private fps: number = 0;
 
   constructor(private ctx: CanvasRenderingContext2D) {}
 
@@ -20,7 +22,17 @@ export default class Simulation {
     this.shouldStop = true;
   }
 
-  gameLoop() {
+  private calculateFps() {
+    const now = performance.now();
+    while (this.times.length > 0 && this.times[0] <= now - 1000) {
+      this.times.shift();
+    }
+    this.times.push(now);
+    this.fps = this.times.length;
+  }
+
+  private gameLoop() {
+    this.calculateFps();
     this.solver.update(frameTime);
     clearScreen(this.ctx);
     // Draw constraint area
@@ -30,6 +42,8 @@ export default class Simulation {
     this.ctx.fillStyle = 'white';
     this.ctx.font = '20px Arial';
     this.ctx.fillText(`Balls: ${count}`, 10, 30);
+    this.ctx.font = '13px Arial';
+    this.ctx.fillText(`FPS: ${this.fps}`, 10, 50);
     // Draw balls
     for (let i = 0; i < count; i++) {
       const ball = this.solver.get(i) as VerletCircle;
@@ -45,7 +59,7 @@ export default class Simulation {
     if (Date.now() - this.lastSpawn > spawnTime) {
       this.lastSpawn = Date.now();
       this.solver.addObject(
-        new VerletCircle(500, 150, randomInt(10, 20), randomColor())
+        new VerletCircle(500, 150, randomInt(4, 8), randomColor())
       );
     }
     if (this.shouldStop) return;
